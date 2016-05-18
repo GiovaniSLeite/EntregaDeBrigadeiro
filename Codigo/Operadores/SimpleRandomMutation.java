8bb5445344323bf2fcf416037d6882cf377f75b5
Giovani Sousa <sousa_giovani@hotmail.com>
2016-05-18 02:44:14 -0300

    Novo operador de Mutação
    Adição do novo operador de mutação.
    Refatoração de parte do código do CrossoverSimpleRandom para reutilização.

---
+++ Codigo/Operadores/SimpleRandomMutation.java
@@ -0,0 +1,38 @@
+
+package Operadores;
+
+import algoritmoGenetico.Utils;
+import java.util.Random;
+
+public class SimpleRandomMutation extends Mutacao{
+    double dist[][];
+    Random rand;
+    double ProbEspecial;
+    public SimpleRandomMutation(double[][] distancias, double prob){    
+        rand = new Random();
+        dist = distancias;
+        ProbEspecial = prob;
+    }
+    
+
+    @Override
+    public int[] executar(int[] individuo) {
+        
+        int p = individuo[rand.nextInt(individuo.length)];
+        int [] mutante;
+        if(p != -1)
+            mutante = individuo;
+        else{
+            mutante = Utils.removeClientes(individuo, p);
+            if(rand.nextDouble() <= ProbEspecial)
+                mutante = Utils.inserirMelhorLugar(mutante, p, dist);
+            else
+                mutante = Utils.inserirAleatorio(individuo, p, rand.nextInt(individuo.length+1));
+        }
+            
+        return mutante;
+    }
+    
+    
+    
+}
